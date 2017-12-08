af7f1a6d54b353311217b86dee33dafbccb2e9b3
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-12-08 19:42:19 +0530

    Ui Added login adjusted according to it.

---
+++ public/assets/scripts/klorofil-common.js
@@ -0,0 +1,346 @@
+$(document).ready(function() {
+
+
+	$(function() {
+		var data, options;
+
+		// headline charts
+		data = {
+			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+			series: [
+				[23, 29, 24, 40, 25, 24, 35],
+				[14, 25, 18, 34, 29, 38, 44],
+			]
+		};
+
+		options = {
+			height: 300,
+			showArea: true,
+			showLine: false,
+			showPoint: false,
+			fullWidth: true,
+			axisX: {
+				showGrid: false
+			},
+			lineSmooth: false,
+		};
+
+		new Chartist.Line('#headline-chart', data, options);
+
+
+		// visits trend charts
+		data = {
+			labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+			series: [{
+				name: 'series-real',
+				data: [200, 380, 350, 320, 410, 450, 570, 400, 555, 620, 750, 900],
+			}, {
+				name: 'series-projection',
+				data: [240, 350, 360, 380, 400, 450, 480, 523, 555, 600, 700, 800],
+			}]
+		};
+
+		options = {
+			fullWidth: true,
+			lineSmooth: false,
+			height: "270px",
+			low: 0,
+			high: 'auto',
+			series: {
+				'series-projection': {
+					showArea: true,
+					showPoint: false,
+					showLine: false
+				},
+			},
+			axisX: {
+				showGrid: false,
+
+			},
+			axisY: {
+				showGrid: false,
+				onlyInteger: true,
+				offset: 0,
+			},
+			chartPadding: {
+				left: 20,
+				right: 20
+			}
+		};
+
+		new Chartist.Line('#visits-trends-chart', data, options);
+
+
+		// visits chart
+		data = {
+			labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+			series: [
+				[6384, 6342, 5437, 2764, 3958, 5068, 7654]
+			]
+		};
+
+		options = {
+			height: 300,
+			axisX: {
+				showGrid: false
+			},
+		};
+
+		new Chartist.Bar('#visits-chart', data, options);
+
+
+		// real-time pie chart
+		var sysLoad = $('#system-load').easyPieChart({
+			size: 130,
+			barColor: function(percent) {
+				return "rgb(" + Math.round(200 * percent / 100) + ", " + Math.round(200 * (1.1 - percent / 100)) + ", 0)";
+			},
+			trackColor: 'rgba(245, 245, 245, 0.8)',
+			scaleColor: false,
+			lineWidth: 5,
+			lineCap: "square",
+			animate: 800
+		});
+
+		var updateInterval = 3000; // in milliseconds
+
+		setInterval(function() {
+			var randomVal;
+			randomVal = getRandomInt(0, 100);
+
+			sysLoad.data('easyPieChart').update(randomVal);
+			sysLoad.find('.percent').text(randomVal);
+		}, updateInterval);
+
+		function getRandomInt(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+	});
+
+	/*-----------------------------------/
+	/*	TOP NAVIGATION AND LAYOUT
+	/*----------------------------------*/
+	$('.btn-toggle-fullwidth').on('click', function() {
+		if(!$('body').hasClass('layout-fullwidth')) {
+			$('body').addClass('layout-fullwidth');
+
+		} else {
+			$('body').removeClass('layout-fullwidth');
+			$('body').removeClass('layout-default'); // also remove default behaviour if set
+		}
+
+		$(this).find('.lnr').toggleClass('lnr-arrow-left-circle lnr-arrow-right-circle');
+
+		if($(window).innerWidth() < 1025) {
+			if(!$('body').hasClass('offcanvas-active')) {
+				$('body').addClass('offcanvas-active');
+			} else {
+				$('body').removeClass('offcanvas-active');
+			}
+		}
+	});
+
+	$(window).on('load', function() {
+		if($(window).innerWidth() < 1025) {
+			$('.btn-toggle-fullwidth').find('.icon-arrows')
+			.removeClass('icon-arrows-move-left')
+			.addClass('icon-arrows-move-right');
+		}
+
+		// adjust right sidebar top position
+		$('.right-sidebar').css('top', $('.navbar').innerHeight());
+
+		// if page has content-menu, set top padding of main-content
+		if($('.has-content-menu').length > 0) {
+			$('.navbar + .main-content').css('padding-top', $('.navbar').innerHeight());
+		}
+
+		// for shorter main content
+		if($('.main').height() < $('#sidebar-nav').height()) {
+			$('.main').css('min-height', $('#sidebar-nav').height());
+		}
+	});
+
+
+	/*-----------------------------------/
+	/*	SIDEBAR NAVIGATION
+	/*----------------------------------*/
+
+	$('.sidebar a[data-toggle="collapse"]').on('click', function() {
+		if($(this).hasClass('collapsed')) {
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+		}
+	});
+
+	if( $('.sidebar-scroll').length > 0 ) {
+		$('.sidebar-scroll').slimScroll({
+			height: '95%',
+			wheelStep: 2,
+		});
+	}
+
+
+	/*-----------------------------------/
+	/*	PANEL FUNCTIONS
+	/*----------------------------------*/
+
+	// panel remove
+	$('.panel .btn-remove').click(function(e){
+
+		e.preventDefault();
+		$(this).parents('.panel').fadeOut(300, function(){
+			$(this).remove();
+		});
+	});
+
+	// panel collapse/expand
+	var affectedElement = $('.panel-body');
+
+	$('.panel .btn-toggle-collapse').clickToggle(
+		function(e) {
+			e.preventDefault();
+
+			// if has scroll
+			if( $(this).parents('.panel').find('.slimScrollDiv').length > 0 ) {
+				affectedElement = $('.slimScrollDiv');
+			}
+
+			$(this).parents('.panel').find(affectedElement).slideUp(300);
+			$(this).find('i.lnr-chevron-up').toggleClass('lnr-chevron-down');
+		},
+		function(e) {
+			e.preventDefault();
+
+			// if has scroll
+			if( $(this).parents('.panel').find('.slimScrollDiv').length > 0 ) {
+				affectedElement = $('.slimScrollDiv');
+			}
+
+			$(this).parents('.panel').find(affectedElement).slideDown(300);
+			$(this).find('i.lnr-chevron-up').toggleClass('lnr-chevron-down');
+		}
+	);
+
+
+	/*-----------------------------------/
+	/*	PANEL SCROLLING
+	/*----------------------------------*/
+
+	if( $('.panel-scrolling').length > 0) {
+		$('.panel-scrolling .panel-body').slimScroll({
+			height: '430px',
+			wheelStep: 2,
+		});
+	}
+
+	if( $('#panel-scrolling-demo').length > 0) {
+		$('#panel-scrolling-demo .panel-body').slimScroll({
+			height: '175px',
+			wheelStep: 2,
+		});
+	}
+
+	/*-----------------------------------/
+	/*	TODO LIST
+	/*----------------------------------*/
+
+	$('.todo-list input').change( function() {
+		if( $(this).prop('checked') ) {
+			$(this).parents('li').addClass('completed');
+		}else {
+			$(this).parents('li').removeClass('completed');
+		}
+	});
+
+
+	/*-----------------------------------/
+	/* TOASTR NOTIFICATION
+	/*----------------------------------*/
+
+	if($('#toastr-demo').length > 0) {
+		toastr.options.timeOut = "false";
+		toastr.options.closeButton = true;
+		toastr['info']('Hi there, this is notification demo with HTML support. So, you can add HTML elements like <a href="#">this link</a>');
+
+		$('.btn-toastr').on('click', function() {
+			$context = $(this).data('context');
+			$message = $(this).data('message');
+			$position = $(this).data('position');
+
+			if($context == '') {
+				$context = 'info';
+			}
+
+			if($position == '') {
+				$positionClass = 'toast-left-top';
+			} else {
+				$positionClass = 'toast-' + $position;
+			}
+
+			toastr.remove();
+			toastr[$context]($message, '' , { positionClass: $positionClass });
+		});
+
+		$('#toastr-callback1').on('click', function() {
+			$message = $(this).data('message');
+
+			toastr.options = {
+				"timeOut": "300",
+				"onShown": function() { alert('onShown callback'); },
+				"onHidden": function() { alert('onHidden callback'); }
+			}
+
+			toastr['info']($message);
+		});
+
+		$('#toastr-callback2').on('click', function() {
+			$message = $(this).data('message');
+
+			toastr.options = {
+				"timeOut": "10000",
+				"onclick": function() { alert('onclick callback'); },
+			}
+
+			toastr['info']($message);
+
+		});
+
+		$('#toastr-callback3').on('click', function() {
+			$message = $(this).data('message');
+
+			toastr.options = {
+				"timeOut": "10000",
+				"closeButton": true,
+				"onCloseClick": function() { alert('onCloseClick callback'); }
+			}
+
+			toastr['info']($message);
+		});
+	}
+});
+
+// toggle function
+$.fn.clickToggle = function( f1, f2 ) {
+	return this.each( function() {
+		var clicked = false;
+		$(this).bind('click', function() {
+			if(clicked) {
+				clicked = false;
+				return f2.apply(this, arguments);
+			}
+
+			clicked = true;
+			return f1.apply(this, arguments);
+		});
+	});
+	
+}
+
+function notifyToast(){
+	alert('s');
+}
+
+
