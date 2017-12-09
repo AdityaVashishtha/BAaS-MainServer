3bd04ad3478f149bd41211a442f2f9ce1a0e2e87
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-12-09 17:35:24 +0000

    Database local added for local work

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    database:'mongodb://admin:123456789@ds133496.mlab.com:33496/baas'    
+    database:'mongodb://admin:123456789@ds133496.mlab.com:33496/baas',
+    local:'mongodb://localhost:27017/starter1'
   }
